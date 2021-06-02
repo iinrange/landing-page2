e8486b1bc218bda9e2e2e346a7c27b7d1133c56b
Pablo Gutierrez <benji.pab@gmail.com>
2021-06-02 14:27:34 -0400

    first commit

---
+++ src/sections/team-section.js
@@ -0,0 +1,46 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import { Container, Grid } from 'theme-ui';
+import SectionHeader from 'components/section-header';
+import TeamCard from 'components/team-card';
+
+import {teamsec} from '../data/teamsec'
+
+export default function TeamSection() {
+  return (
+    <section>
+      <Container>
+        <SectionHeader
+          slogan='our team'
+          title='The most qualified and dedicated individuals'
+        />
+          <Grid sx={styles.grid}>
+            {teamsec.map((item)=> (
+              <TeamCard
+                key={item.id}
+                src={item.imgSrc}
+                altText={item.altText}
+                title={item.title}
+                designation={item.designation}
+                social={item.socialProfile}              
+              />
+            ))}
+          </Grid>
+      </Container>
+    </section>
+  );
+}
+
+const styles = {
+  grid: {
+    mt: [0, null, -6, null, -4],
+    gridGap: ['35px 0px', null, 0, null, null, '30px 35px'],
+    gridTemplateColumns: [
+      'repeat(2,1fr)',
+      null,
+      'repeat(2,1fr)',
+      null,
+      'repeat(3,1fr)',
+    ],
+  },
+};
